acac16743e57b21a5a42d977d61da16a574df0c1
cristianpiuca <cristianalexanderp1995@gmail.com>
2022-09-12 19:12:11 -0300

    end

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3031;
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req,res) => res.sendFile(path.resolve(__dirname, "views", "home.html")));
+
+app.get("/ofertas", (req,res) => res.sendFile(path.resolve(__dirname, "views", "ofertas.html")));
+
+app.get("/tienda", (req,res) => res.sendFile(path.resolve(__dirname, "views", "tienda.html")));
+
+app.get("/vender", (req,res) => res.sendFile(path.resolve(__dirname, "views", "vender.html")));
+
+app.get("/ayuda", (req,res) => res.sendFile(path.resolve(__dirname, "views", "ayuda.html")));
+
+app.get("/register", (req,res) => res.sendFile(path.resolve(__dirname, "views", "register.html")));
+
+app.get("/login", (req,res) => res.sendFile(path.resolve(__dirname, "views", "login.html")));
+
+app.get("/compras", (req,res) => res.sendFile(path.resolve(__dirname, "views", "compras.html")));
+
+app.listen(port, () => console.log(`Servidor corriendo en el puerto 3030 http://localhost:${port}`))
